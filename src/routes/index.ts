4556bdc15bca072e2bd4529ee31ae0853f8cfd19
Rafael de Miranda <37124918+rafaeldemiranda95@users.noreply.github.com>
2024-01-29 19:28:51 -0300

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,9 +10,9 @@ const usuarioUseCase = new UsuarioUseCase();
 router.get('/', (req, res) => {
   res.status(200).send('OK');
 });
-router.get('/', (req, res) => {
-  res.status(200).send('OK');
-});
+// router.get('/', (req, res) => {
+//   res.status(200).send('OK');
+// });
 
 router.post(
   '/cadastroProduto',
